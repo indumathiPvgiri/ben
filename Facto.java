f58c69d42f96cb910fe4b6bd8a41582a92776bd3
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-05 18:04:21 +0530

    Update Facto.java

--- Facto.java
+++ Facto.java
@@ -7,10 +7,18 @@ class Facto
         Scanner sc=new Scanner(System.in);
         int b=sc.nextInt();
         int i,fact=1;
+        if(b>=0)
+        {
         for(i=1;i<=b;i++)
         {
-            fact=fact*i;
+             fact=fact*i;
+            }
+            System.out.println(fact);
         }
-    System.out.println(fact);
+            else
+            {
+                System.out.println("invalid");
+            }
+  
     }
-}
+    }
