bab8e11b91efb93b56aff5aafc2303ce60c64413
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-06 14:44:52 +0530

    Create Strnt.java

---
+++ Strnt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Strnt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int a=Integer.parseInt(s1);
+        System.out.println(a+s1);
+    }
+}
