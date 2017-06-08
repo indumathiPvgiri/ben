37b52ad88c61260631bfa6760671c83098b8c580
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-08 20:56:44 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class Str
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+String s2[]=s1.split(" ");
+for(int i=s2.length-1;i>=0;i--)
+{
+System.out.print(s2[i]+" ");
+}
+}
+}
