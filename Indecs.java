dd2b589cb3308e0c49c4c8df75a5d810fa11608a
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-09 05:57:17 -0700

    Create Indecs.java

--- Indecs.java
+++ Indecs.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
-int b[]=new int[n];
+int b[]=new int[a];
 for(int i=0;i<b.length;i++)
 {
 b[i]=sc.nextInt();
