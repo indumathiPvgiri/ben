ae5fb954c26a914fa22684041a641166bd5f2988
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-09 05:56:17 -0700

    Create Indecs.java

---
+++ Indecs.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Indecs
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b[]=new int[n];
+for(int i=0;i<b.length;i++)
+{
+b[i]=sc.nextInt();
+}
+Arrays.sort(b);
+for(int i=0;i<b.length;i++)
+{
+if(b[i]==i)
+{
+System.out.print(b[i]);
+}
+}
+}}
