0640296b4ab5ac1e2c1bbb16477227dbb610eed4
indumathiPvgiri <induvarugiri@gmail.com>
2017-06-19 21:18:13 +0530

    Create Odeven.java

---
+++ Odeven.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class Odeven
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a%2!=0)
+    {
+        System.out.println("Odd");
+    }
+    else
+    {
+        System.out.println("Even");
+    }
+    
+    }
+    
+}
+
